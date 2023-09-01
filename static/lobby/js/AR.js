5b00da92c63a5fb089fa1ea39210273507dc3066
BlackShrike <qw991012@gmail.com>
2023-09-02 08:54:38 +0900

    부가 기능 예시

--- static/lobby/js/AR.js
+++ static/lobby/js/AR.js
@@ -1,13 +1,12 @@
-let chatSocket; 
+let chatSocket;
 let room_name;
 let selectedFriend;
 
-const urlParts = window.location.pathname.split('/');
+const urlParts = window.location.pathname.split("/");
 const username = urlParts[urlParts.length - 2];
 const ws_protocol = window.location.protocol === "https:" ? "wss://" : "ws://";
 
-let friends=[];
-
+let friends = [];
 
 document.addEventListener("DOMContentLoaded", function () {
   //scene,UI 객체화
@@ -16,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 채팅버튼 객체화
   let chatbutton = document.querySelector("#chatbutton");
   let button2 = document.querySelector("#button2");
-  let button3 = document.querySelector("#button3");
+  let Miscbutton = document.querySelector("#Miscbutton");
 
   let friend = document.querySelector("#friend");
   let profile = document.querySelector("#profile");
@@ -45,17 +44,16 @@ document.addEventListener("DOMContentLoaded", function () {
   //채팅,UI 시각화 관련 변수 전역화
   let isUIVisible = true;
   let isChatVisible = false;
-
+  let isMiscVisible = false;
   function initializeFriends() {
     // Ajax를 이용해 서버에서 친구 목록과 대화를 가져옴
-    fetch('/get_friends_and_conversations/')
-      .then(response => response.json())
-      .then(data => {
+    fetch("/get_friends_and_conversations/")
+      .then((response) => response.json())
+      .then((data) => {
         friends = data.friends;
-        displayFriends();  // 친구 목록을 화면에 표시
+        displayFriends(); // 친구 목록을 화면에 표시
       });
   }
-
 
   scene.addEventListener("enter-vr", function () {
     ui.setAttribute("visible", "true");
@@ -74,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //채팅창 띄우기 및 감추기
     friend.setAttribute("visible", !isChatVisible);
     button2.setAttribute("visible", isChatVisible);
-    button3.setAttribute("visible", isChatVisible);
+    Miscbutton.setAttribute("visible", isChatVisible);
     profile.setAttribute("visible", isChatVisible);
     if (isChatVisible == false) {
       enableChatButtons();
@@ -100,6 +98,25 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     ui.setAttribute("visible", !isUIVisible);
     isUIVisible = !isUIVisible;
+  }
+  function toggleMisc() {
+    // Misc 화면 띄우기 및 감추기
+    Misc.setAttribute("visible", !isMiscVisible);
+    button2.setAttribute("visible", isMiscVisible);
+    chatbutton.setAttribute("visible", isMiscVisible);
+    profile.setAttribute("visible", isMiscVisible);
+
+    if (isMiscVisible == false) {
+      enableMiscButtons();
+    } else {
+      enableUIButtons();
+    }
+
+    isMiscVisible = !isMiscVisible;
+
+    if (isMiscVisible) {
+      displayMisc();
+    }
   }
   function pauseall(entity) {
     // 엔티티와 그 자식들을 일시 중지
@@ -185,7 +202,28 @@ document.addEventListener("DOMContentLoaded", function () {
       button.setAttribute("visible", "true");
     });
   }
-
+  function enableMiscButtons() {
+    //Miscbutton 버튼으로 전환
+    const buttonIds = [
+      "up",
+      "down",
+      "left",
+      "right",
+      "forward",
+      "backward",
+      "recordButton",
+      "sttText",
+      "recordText",
+      "input-button",
+      "eraser-button",
+    ];
+    buttonIds.forEach((buttonId) => {
+      const button = document.getElementById(buttonId);
+      button.removeEventListener("click", moveUI);
+      button.addEventListener("click", moveFeature);
+      button.setAttribute("visible", "true");
+    });
+  }
   function movechat(event) {
     switch (event.target.id) {
       case "up":
@@ -256,12 +294,57 @@ document.addEventListener("DOMContentLoaded", function () {
     // 실제 UI의 위치를 변경
     ui.setAttribute("position", positionToString(currentUIPosition));
   }
-
+  function moveFeature(event) {
+    //부가기능 움직이기
+    switch (event.target.id) {
+      case "up":
+        if (selectedIndex > 0) {
+          selectedIndex--;
+          displayMisc();
+        }
+        break;
+      case "down":
+        if (selectedIndex < itemsPerPage - 1) {
+          selectedIndex++;
+          displayMisc();
+        }
+        break;
+      case "left":
+        if (currentFeaturePage > 0) {
+          currentFeaturePage--;
+          displayMisc();
+        }
+        break;
+      case "right":
+        if (
+          (currentFeaturePage + 1) * itemsPerPage <
+          displayMiscFeatures.length
+        ) {
+          // 수정된 부분
+          currentFeaturePage++;
+          displayMisc();
+        }
+        break;
+      case "forward":
+        displayMiscdetail();
+        break;
+      case "backward":
+        const miscContainer = document.getElementById("MiscContainer"); // 수정된 부분
+        while (miscContainer.firstChild) {
+          miscContainer.removeChild(miscContainer.firstChild);
+        }
+        displayMisc();
+        break;
+      default:
+        break;
+    }
+  }
   let currentPage = 0;
   let selectedIndex = 0;
   const itemsPerPage = 5;
 
   function displayFriends() {
+    selectedIndex = 0;
     const start = currentPage * itemsPerPage;
     const end = start + itemsPerPage;
     const currentFriends = friends.slice(start, end);
@@ -300,14 +383,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const friendsContainer = document.getElementById("friendsContainer");
     const friendList = document.getElementById("friendList");
 
-     // friendList가 있는지 확인
-     if (friendList) {
+    // friendList가 있는지 확인
+    if (friendList) {
       friendList.setAttribute("visible", "false");
     }
 
-       // 이전 내용 지우기
+    // 이전 내용 지우기
     while (friendsContainer.firstChild) {
-        friendsContainer.removeChild(friendsContainer.firstChild);
+      friendsContainer.removeChild(friendsContainer.firstChild);
     }
 
     const conversations = document.createElement("a-entity");
@@ -326,8 +409,8 @@ document.addEventListener("DOMContentLoaded", function () {
     while (friendsContainer.firstChild) {
       friendsContainer.removeChild(friendsContainer.firstChild);
     }
-    
-    const messages = conversation.split('\n').slice(-5);  // 마지막 5개의 메시지만 가져옴
+
+    const messages = conversation.split("\n").slice(-5); // 마지막 5개의 메시지만 가져옴
     messages.forEach((message, index) => {
       const newMessageEntity = document.createElement("a-entity");
       newMessageEntity.setAttribute(
@@ -339,13 +422,233 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  
+  const displayMiscFeatures = [
+    {
+      name: "GPT",
+    },
+    // ... 다른 항목들 ...
+  ];
+  let currentFeaturePage = 0;
+  let totalFeaturePages = 0;
 
+  function displayMisc() {
+    selectedIndex = 0;
+    const start = currentFeaturePage * itemsPerPage;
+    const end = start + itemsPerPage;
+    const currentdisplayMisc = displayMiscFeatures.slice(start, end); // 수정된 부분
+
+    const miscContainer = document.getElementById("MiscContainer");
+    while (miscContainer.firstChild) {
+      miscContainer.removeChild(miscContainer.firstChild);
+    }
+
+    currentdisplayMisc.forEach((feature, index) => {
+      const entity = document.createElement("a-entity");
+      entity.setAttribute(
+        "text",
+        `value: ${feature.name}; color: white; align: center;`
+      );
+      entity.setAttribute("position", `0 ${0.07 * (2 - index)} 0`);
+
+      if (index === selectedIndex) {
+        entity.setAttribute("text", `color: yellow`);
+        const animation = document.createElement("a-animation");
+        animation.setAttribute("attribute", "material.opacity");
+        animation.setAttribute("from", "0.5");
+        animation.setAttribute("to", "1");
+        animation.setAttribute("dur", "1500");
+        animation.setAttribute("repeat", "indefinite");
+        animation.setAttribute("direction", "alternate");
+        entity.appendChild(animation);
+      }
+      miscContainer.appendChild(entity);
+    });
+  }
+
+  function displayMiscdetail() {
+    const selectedFeature =
+      displayMiscFeatures[currentFeaturePage * itemsPerPage + selectedIndex];
+    const miscContainer = document.getElementById("MiscContainer");
+
+    while (miscContainer.firstChild) {
+      miscContainer.removeChild(miscContainer.firstChild);
+    }
+
+    // "GPT" 항목이 선택된 경우
+    if (selectedFeature.name === "GPT") {
+      GPTQuestion();
+      return; // 추가된 부분: GPTQuestion 함수를 실행한 후 함수를 종료
+    }
+
+    // 다른 항목들에 대한 처리 (예: 친구 목록 표시 등)
+  }
+  let currentQuestionPage = 0;
+  let currentAnswerPage = 0;
+
+  function GPTQuestion() {
+    const miscContainer = document.getElementById("MiscContainer");
+
+    const maxCharsPerLine = 20; // 예: 각 줄에 20자까지만 표시
+    // 임의의 장문 질문
+    const longQuestion =
+      "왜 우주는 존재하는 것일까요? 우주의 시작과 끝, 그리고 그 안에 존재하는 모든 것들은 어떻게 형성되었을까요? 우리는 왜 여기에 있는 것일까요? 인간의 존재의 의미는 무엇일까요?";
+
+    // 임의의 장문 대답
+    const longAnswer =
+      "우주의 존재와 시작에 대한 질문은 과학자들 사이에서도 아직 확실한 답을 찾지 못한 미스터리 중 하나입니다. 빅뱅 이론은 우주의 시작을 설명하는 가장 널리 받아들여진 이론 중 하나입니다. 인간의 존재와 그 의미에 대해서는 철학, 종교, 과학 등 다양한 분야에서 다양한 해석이 있습니다. 인간의 존재의 의미를 찾는 것은 개인의 여정이며, 각자의 경험과 신념에 따라 다를 수 있습니다.";
+
+    // 텍스트를 여러 페이지로 분할하는 함수
+    function paginateText(text, charsPerLine, linesPerPage) {
+      const words = text.split(" ");
+      let lines = [];
+      let currentLine = "";
+
+      words.forEach((word) => {
+        if ((currentLine + word).length > charsPerLine) {
+          lines.push(currentLine.trim());
+          currentLine = "";
+        }
+        currentLine += word + " ";
+      });
+      if (currentLine) lines.push(currentLine.trim());
+
+      let pages = [];
+      for (let i = 0; i < lines.length; i += linesPerPage) {
+        pages.push(lines.slice(i, i + linesPerPage).join("\n"));
+      }
+
+      return pages;
+    }
+
+    const questionPages = paginateText(longQuestion, maxCharsPerLine, 2);
+    const answerPages = paginateText(longAnswer, maxCharsPerLine, 5);
+
+    const totalQuestionPages = questionPages.length;
+    const totalAnswerPages = answerPages.length;
+
+    // 질문 칸 생성
+    const questionEntity = document.createElement("a-entity");
+    questionEntity.setAttribute("id", "question-text");
+    questionEntity.setAttribute(
+      "text",
+      `value: ${questionPages[currentQuestionPage]}; color: white; align: center; width: 0.35;`
+    );
+    questionEntity.setAttribute(
+      "geometry",
+      "primitive: plane; width: 0.30; height: 0.1"
+    );
+    questionEntity.setAttribute("position", `-0.025 0.05 0.01`); // z값 조절
+    miscContainer.appendChild(questionEntity);
+
+    // 대답 칸 생성
+    const answerEntity = document.createElement("a-entity");
+    answerEntity.setAttribute("id", "answer");
+    answerEntity.setAttribute(
+      "text",
+      `value: Answer: ${answerPages[currentAnswerPage]}; color: white; align: center; width: 0.35;`
+    );
+    answerEntity.setAttribute(
+      "geometry",
+      "primitive: plane; width: 0.30; height: 0.25"
+    );
+    answerEntity.setAttribute("position", `-0.025 -0.15 0.01`); // 수정된 부분
+    miscContainer.appendChild(answerEntity);
+    //버튼 만들기 펑션
+    function createButton(textValue, position, clickCallback) {
+      const buttonEntity = document.createElement("a-entity");
+      buttonEntity.setAttribute(
+        "geometry",
+        "primitive: plane; width: 0.1; height: 0.05"
+      );
+      buttonEntity.setAttribute("material", "color: #333");
+      buttonEntity.setAttribute("position", position);
+      buttonEntity.setAttribute("class", "clickable");
+      buttonEntity.addEventListener("click", clickCallback);
+
+      const textEntity = document.createElement("a-entity");
+      textEntity.setAttribute(
+        "text",
+        `value: ${textValue}; color: white; align: center; width: 0.5;` // width를 0.09로 조절하여 텍스트 크기를 조정
+      );
+      textEntity.setAttribute("position", "0 0 0.01"); // Slightly in front of the button plane for visibility
+      buttonEntity.appendChild(textEntity);
+
+      return buttonEntity;
+    }
+    // 질문에 대한 페이지네이션 버튼
+    const questionPrevButton = createButton(
+      "이전",
+      `0.175 0.05 0.01`,
+      function () {
+        if (currentQuestionPage > 0) {
+          currentQuestionPage--;
+          updateQuestionPage();
+        }
+      }
+    );
+    questionEntity.appendChild(questionPrevButton);
+
+    const questionNextButton = createButton(
+      "다음",
+      `0.175 -0.05 0.01`,
+      function () {
+        if (currentQuestionPage < totalQuestionPages - 1) {
+          currentQuestionPage++;
+          updateQuestionPage();
+        }
+      }
+    );
+    questionEntity.appendChild(questionNextButton);
+
+    const answerPrevButton = createButton(
+      "이전",
+      `0.175 0.05 0.01`,
+      function () {
+        if (currentAnswerPage > 0) {
+          currentAnswerPage--;
+          updateAnswerPage();
+        }
+      }
+    );
+    answerEntity.appendChild(answerPrevButton);
+
+    const answerNextButton = createButton(
+      "다음",
+      `0.175 -0.05 0.01`,
+      function () {
+        if (currentAnswerPage < totalAnswerPages - 1) {
+          currentAnswerPage++;
+          updateAnswerPage();
+        }
+      }
+    );
+    answerEntity.appendChild(answerNextButton);
+    //페이지 업데이트
+    function updateQuestionPage() {
+      const questionEntity = document.getElementById("question-text");
+      if (questionEntity) {
+        questionEntity.setAttribute(
+          "text",
+          `value: ${questionPages[currentQuestionPage]}; color: white; align: center; width: 0.35;`
+        );
+      }
+    }
+
+    function updateAnswerPage() {
+      const answerEntity = document.getElementById("answer");
+      if (answerEntity) {
+        answerEntity.setAttribute(
+          "text",
+          `value: Answer: ${answerPages[currentAnswerPage]}; color: white; align: center; width: 0.35;`
+        );
+      }
+    }
+  }
   window.addEventListener("DOMContentLoaded", function () {
     var arButton = document.querySelector(".a-enter-ar-button");
     if (arButton) {
       arButton.disabled = true;
-      fetchFriendsAndConversations(); 
+      fetchFriendsAndConversations();
     }
   });
   scene.addEventListener("loaded", function () {
@@ -374,11 +677,14 @@ document.addEventListener("DOMContentLoaded", function () {
         hideUI.setAttribute("visible", "false");
       });
     }
-    chatbutton.addEventListener("click", function() {
-      initializeFriends();  // 친구 목록 초기화
+    chatbutton.addEventListener("click", function () {
+      initializeFriends(); // 친구 목록 초기화
       toggleChat();
     });
     hideUI.addEventListener("click", toggleUIVisibility);
+    Miscbutton.addEventListener("click", function () {
+      toggleMisc();
+    });
     enableUIButtons();
   });
 
@@ -388,50 +694,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
   inputButton.addEventListener("click", function () {
     if (chatSocket) {
-        chatSocket.close();
+      chatSocket.close();
     }
     if (!selectedFriend) {
       console.error("selectedFriend이 undefined입니다.");
       return;
     }
 
-    room_name = selectedFriend.username < username ? `${selectedFriend.username}_${username}` : `${username}_${selectedFriend.username}`;
-    chatSocket = new WebSocket(ws_protocol + window.location.host + '/ws/chat/' + room_name + '/');
+    room_name =
+      selectedFriend.username < username
+        ? `${selectedFriend.username}_${username}`
+        : `${username}_${selectedFriend.username}`;
+    chatSocket = new WebSocket(
+      ws_protocol + window.location.host + "/ws/chat/" + room_name + "/"
+    );
 
-    chatSocket.onmessage = function(e) {
+    chatSocket.onmessage = function (e) {
       const data = JSON.parse(e.data);
-      if (data.message_type === 'new_message') {
+      if (data.message_type === "new_message") {
         // 실시간으로 채팅 업데이트
         selectedFriend.conversation += `\n${data.sender}: ${data.message}`;
         updateConversation(selectedFriend.conversation);
       }
-  };
-   
-    chatSocket.onclose = function(e) {
-        console.error('Chat socket closed unexpectedly');
     };
 
-    chatSocket.onerror = function(e) {
-        console.error('Chat socket encountered an error');
+    chatSocket.onclose = function (e) {
+      console.error("Chat socket closed unexpectedly");
     };
 
-    chatSocket.addEventListener('open', function() {
-        const message = sttText.getAttribute("value");
-        const sender = username;
+    chatSocket.onerror = function (e) {
+      console.error("Chat socket encountered an error");
+    };
 
-        if (message) {
-            chatSocket.send(JSON.stringify({
-                'message': message,
-                'sender': sender,
-            }));
-        }
-        selectedFriend.conversation += `\n${data.sender}: ${data.message}`;
-        updateConversation(selectedFriend.conversation);
+    chatSocket.addEventListener("open", function () {
+      const message = sttText.getAttribute("value");
+      const sender = username;
+
+      if (message) {
+        chatSocket.send(
+          JSON.stringify({
+            message: message,
+            sender: sender,
+          })
+        );
+      }
+      selectedFriend.conversation += `\n${data.sender}: ${data.message}`;
+      updateConversation(selectedFriend.conversation);
     });
 
     // "초기화" 메시지 처리
     if (message === "초기화") {
-        toggleUIVisibility();
+      toggleUIVisibility();
     }
   });
   /** 한글 폰트 지정 */
